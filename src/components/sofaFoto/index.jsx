952718c661ddcd7e7f00c6d3ed15c2d85016a957
Rita <ritamitsuhashi@gmail.com>
2023-08-04 14:26:49 -0300

    added card picture and text

--- src/components/sofaFoto/index.jsx
+++ src/components/sofaFoto/index.jsx
@@ -1,9 +1,8 @@
 import styles from './SofaFoto.module.css';
-import sofaEstatico from '/assets/card-sofa-estatico.png';
 
 export default function SofaFoto() {
     return(
      //<img className={styles.foto} style={{ backgroundImage: `url('./assets/card-sofa-${imagem}.png')` }} />
-     <img src={sofaEstatico} alt="Imagem Sofá Margot II Rosé" className={styles.foto} />
+     <img src="assets/card-sofa-estatico.png" alt="Imagem Sofá Margot II Rosé" className={styles.foto} />
     )
 }
--- src/components/sofaTexto/index.jsx
+++ src/components/sofaTexto/index.jsx
@@ -2,8 +2,14 @@ import styles from "./SofaTexto.module.css";
 
 export default function SofaTexto() {
     return(
-        <div className={styles.texto}>
-            <h1>Sofá Margot II - Rosé</h1>
+        <>
+        <div className={styles.boxTexto}>
+        <p>CÓD</p>
+        <h1>Sofá Margot II - Rosé</h1>
+        <h2>R$ 4.000</h2>
         </div>
+
+        <button className={styles.botaoComprar}>Adicionar à cesta</button>
+        </>
     )
 }
